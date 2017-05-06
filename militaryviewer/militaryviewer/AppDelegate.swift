042855cb49727ecb0c966214243a7e9448eb10fa
developer <gisfromscratch@live.de>
2017-05-06 08:48:41 +0200

    Added a simple coca app

---
+++ militaryviewer/militaryviewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  militaryviewer
+//
+//  Created by Developer on 06.05.17.
+//  Copyright © 2017 Developer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
